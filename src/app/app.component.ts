401adbdf1909b1c557a9e495e82b17eb38f9d064
alex.paclin <alex.paclin@gmail.com>
2018-05-14 23:17:03 +0500

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>My First Angular App</h1><nav-bar></nav-bar>'  
 })
 export class AppComponent {
   title = 'app';
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  template: 'NAVBAR'  
+})
+export class NavbarComponent {
+  title = 'app';
+}
